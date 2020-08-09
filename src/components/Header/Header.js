242ee9f9d93fd5236dd0b7ca96d0b8110b1def86
Marshall88GB <gasic.boban@gmail.com>
2020-08-09 10:02:45 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+
+      *** Phone Book  by Marshall_GB ***
+    </header>
+  );
+}
+
+export default Header;
